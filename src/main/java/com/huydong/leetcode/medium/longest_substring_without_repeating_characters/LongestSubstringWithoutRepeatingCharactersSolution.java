f58aa3d4e0b1c4943f00c40ba635fbc296833b40
huydong <huydong0210@gmail.com>
2024-06-24 11:17:16 +0700

    longest subtring without repeating solution

--- src/main/java/com/huydong/leetcode/medium/longest_substring_without_repeating_characters/LongestSubstringWithoutRepeatingCharactersSolution.java
+++ src/main/java/com/huydong/leetcode/medium/longest_substring_without_repeating_characters/LongestSubstringWithoutRepeatingCharactersSolution.java
@@ -1,33 +1,32 @@
 package com.huydong.leetcode.medium.longest_substring_without_repeating_characters;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class LongestSubstringWithoutRepeatingCharactersSolution {
+    // sliding window
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) {
+        if (s.length() == 0){
             return 0;
         }
-        int max = 1;
-        int i =0;
-        while (i < s.length()){
-
-
-        }
-
-
-        return max;
-    }
-
-    public Character repeatingCharacter(String s) {
-        Set<Character> characters = new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (!characters.contains(s.charAt(i))) {
-                characters.add(s.charAt(i));
+        Set<Character> characterSet = new HashSet<>();
+        int max=1;
+        int left =0;
+        int right = 1;
+        characterSet.add(s.charAt(0));
+        while (right < s.length()){
+            if (!characterSet.contains(s.charAt(right))){
+                characterSet.add(s.charAt(right));
+                if (characterSet.size() > max){
+                    max = characterSet.size();
+                }
+                right++;
             } else {
-                return s.charAt(i);
+                characterSet.remove(s.charAt(left));
+                left++;
             }
         }
-        return null;
+        return max;
+
     }
+
 }
