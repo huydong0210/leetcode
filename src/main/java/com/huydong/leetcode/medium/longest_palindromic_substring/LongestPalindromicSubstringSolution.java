b36e36bd8ec74de81ac749d80023c99d1a36a91f
huydong <huydong0210@gmail.com>
2024-06-26 11:47:38 +0700

    longest palindromic substring update

--- src/main/java/com/huydong/leetcode/medium/longest_palindromic_substring/LongestPalindromicSubstringSolution.java
+++ src/main/java/com/huydong/leetcode/medium/longest_palindromic_substring/LongestPalindromicSubstringSolution.java
@@ -10,32 +10,21 @@ public class LongestPalindromicSubstringSolution {
         while (i < s.length()) {
             String twoChar = null;
             String threeChar = null;
-            String fourChar = null;
             if (i + 2 <= s.length()) {
                 twoChar = s.substring(i, i + 2);
             }
             if (i + 3 <= s.length()) {
                 threeChar = s.substring(i, i + 3);
             }
-            if (i + 4 <= s.length()) {
-                fourChar = s.substring(i, i + 4);
-            }
             if (Objects.nonNull(twoChar) && twoChar.charAt(0) == twoChar.charAt(1)) {
-                String tmp = findLongestPalindromicAllSameCharacter(s, i, i + 1);
+                String tmp = findLongestPalindromic(s, i, i + 1);
                 if (tmp.length() > max) {
                     max = tmp.length();
                     result = tmp;
                 }
             }
             if (Objects.nonNull(threeChar) && threeChar.charAt(0) == threeChar.charAt(2)) {
-                String tmp = findLongestPalindromicNotAllSameCharacter(s, i, i + 2);
-                if (tmp.length() > max) {
-                    max = tmp.length();
-                    result = tmp;
-                }
-            }
-            if (Objects.nonNull(fourChar) && fourChar.charAt(0) == fourChar.charAt(3) && fourChar.charAt(1) == fourChar.charAt(2)) {
-                String tmp = findLongestPalindromicNotAllSameCharacter(s, i, i + 3);
+                String tmp = findLongestPalindromic(s, i, i + 2);
                 if (tmp.length() > max) {
                     max = tmp.length();
                     result = tmp;
@@ -46,27 +35,7 @@ public class LongestPalindromicSubstringSolution {
         return result;
     }
 
-    private String findLongestPalindromicAllSameCharacter(String s, int left, int right) {
-        while (left > 0 || right < s.length() - 1) {
-            boolean isContinueExpend = false;
-
-            if (left > 0 && s.charAt(left) == s.charAt(left - 1)) {
-                left--;
-                isContinueExpend = true;
-            }
-            if (right < s.length() - 1 && s.charAt(right) == s.charAt(right + 1)) {
-                right++;
-                isContinueExpend = true;
-            }
-            if (!isContinueExpend) {
-                break;
-            }
-
-        }
-        return s.substring(left, right + 1);
-    }
-
-    private String findLongestPalindromicNotAllSameCharacter(String s, int left, int right) {
+    private String findLongestPalindromic(String s, int left, int right) {
         while (left > 0 && right < s.length() - 1) {
             if (s.charAt(left - 1) == s.charAt(right + 1)) {
                 left--;
